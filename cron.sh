9050bdbbb7a6e001e0d290acdb7744536d15cdb4
Connor van Spronssen <info@dobefu.com>
2025-06-08 20:47:31 +0200

    Sun Jun  8 20:47:31 CEST 2025

---
+++ cron.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+$HOME/bin/backup-git-repo $HOME/.config/nvim
+$HOME/bin/backup-git-repo $HOME/.config/zsh
+$HOME/bin/backup-git-repo $HOME/bin
+$HOME/bin/backup-git-repo $HOME/.config/ghostty
+$HOME/bin/backup-git-repo $HOME/dotfiles
